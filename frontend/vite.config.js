4197e033b5f0fa5c125b6df95afc0ffe3b534856
Swapnil Patil <spatil@sandiego.edu>
2026-03-05 00:59:06 -0500

    chore: added github deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/aai590_5_capstone_project/",
   plugins: [react()],
   server: {
     port: 3000,
